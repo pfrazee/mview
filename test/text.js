fdd9d271d0903dccd72b27fa4a062c4c40683201
Paul Frazee <pfrazee@gmail.com>
2014-11-29 13:27:00 -0600

    added sim-test for texts

--- test/text.js
+++ test/text.js
@@ -60,3 +60,54 @@ tape('concurrent updates', function (t) {
 
   t.end()
 })
+
+function sim(numNodes) {
+  var updates = []
+  var texts = []
+  for (var i = 0; i < numNodes; i++)
+    texts.push(mview.text())
+
+  var curTag = 0
+  function pickIndex(n) {
+    return (Math.random()*n)|0
+  }
+  function pickText() {
+    return texts[pickIndex(numNodes)] || texts[0]
+  }
+
+  // iterate A-Z
+  for (var i='A'.charCodeAt(0); i <= 'Z'.charCodeAt(0); i++) {
+    // apply to three texts
+    for (var j=0; j < 3; j++) {
+      var text = pickText()
+      var update = text.diff(text.toString() + ' ' + String.fromCharCode(i))
+
+      // apply the update to the chosen text, then store for the others to run later
+      text.update(update)
+      updates.push(update)
+    }
+  }
+
+  // run all updates on all texts
+  updates.forEach(function(update) {
+    texts.forEach(function(text) {
+      text.update(update)
+    })
+  })  
+
+  return texts
+}
+
+tape('network sim: 2-16 nodes', function(t) {
+  for (var numNodes = 2; numNodes <= 16; numNodes++) {
+    var texts = sim(numNodes)
+
+    console.log(texts[0].toString())
+    for (var j=1; j < numNodes; j++) {
+      console.log(texts[j].toString())
+      t.equal(texts[0].toString(), texts[j].toString())
+    }
+  }
+
+  t.end()
+})
